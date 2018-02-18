cd4844e423da70e320cc74496903d433c23a5968
Yi Zhao <zhaoyi0113@gmail.com>
2018-02-18 14:22:42 +1100

    update port

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 
 const {router} = require('./router');
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 3100;
 
 const app = express();
 
